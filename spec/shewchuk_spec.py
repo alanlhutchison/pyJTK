db5f36476cb8253f896faeff020dcc7b8e33a626
andy c <t2ahc@sad-panda.local>
2012-08-28 09:20:39 -0500

    build shewchuk algorithm methods

---
+++ spec/shewchuk_spec.py
@@ -0,0 +1,18 @@
+#!/usr/bin/epython
+
+import sys
+import os.path
+
+p = os.path.realpath(__file__)
+q = os.path.split(os.path.dirname(p))
+sys.path.append(os.path.join(q[0], "src"))
+del p, q # keep globals clean
+
+import unittest
+import shewchuk as sh
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/shewchuk.py
@@ -0,0 +1,61 @@
+#!/usr/bin/epython
+
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+# http://www.cs.cmu.edu/afs/cs/project/quake/public/papers/robust-arithmetic.ps
+
+def fast_two_sum(a,b):
+    """Assumes abs(a) >= abs(b), otherwise use two_sum."""
+    x = a + b
+    b_virtual = x - a
+    y = b - b_virtual
+    if y == 0:
+        return x
+    else:
+        return (x,y)
+
+def two_sum(a,b):
+    """Evaluates adaptive precision summation for a,b in unknown order"""
+    x = a + b
+    
+    b_virtual = x - a
+    a_virtual = x - b_virtual
+    
+    b_roundoff = b - b_virtual
+    a_roundoff = a - a_virtual
+    
+    y = a_roundoff + b_roundoff
+    
+    if y == 0:
+        return x
+    else:
+        return (x,y)
+
+def expansion_sum(series):
+    series = sorted(series, key=lambda v: abs(v))
+    z = fast_two_sum(series[1], series[0])
+    h = None
+    
+    try:
+        q = z[0]
+        h = z[1]
+    except: # not a tuple
+        q = z
+    
+    if len(series) == 2:
+        return (h,q)
+    
+    for v in series[2:]:
+        z = two_sum(q,v)
+        try:
+            q = z[0]
+            h = z[1]
+        except: # not a tuple
+            q = z
+    
+    return (h,q)
+
+if __name__ == "__main__":
+    print "Adaptive precision summation algorithms."
