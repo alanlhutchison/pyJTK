e4876202110f76e5f99b98a84c014b9ffd5c5c30
andy c <t2ahc@sad-panda.local>
2012-09-11 11:23:13 -0500

    build normal distribution

--- spec/normal_distribution_spec.py
+++ spec/normal_distribution_spec.py
@@ -9,10 +9,46 @@ sys.path.append(os.path.join(q[0], "src"))
 del p, q # keep globals clean
 
 import unittest
+import numpy as np
 import normal_distribution as nd
+from math import sqrt
 
 class DistributionSpec(unittest.TestCase):
-    pass
+    def setUp(self):
+        pass
+    
+    def test_initialization_with_int(self):
+        case = nd.NormalDistribution(6,1)
+        self.assertTrue(isinstance(case,nd.NormalDistribution))
+    
+    def test_initialization_with_array(self):
+        case = nd.NormalDistribution(6,np.array([1.0]))
+        self.assertTrue(isinstance(case,nd.NormalDistribution))
+    
+    def test_score_bounds(self):
+        case = nd.NormalDistribution(10,1.0)
+        
+        c_maximum = 45.0
+        c_expected = 22.5
+        
+        self.assertEqual(case.max_score, c_maximum)
+        self.assertEqual(case.expected, c_expected)
+    
+    def test_standard_deviations(self):
+        case = nd.NormalDistribution(5,2)
+        
+        c_stdev = sqrt((2300. - 5.*28.) / 72.0)
+        
+        self.assertEqual(case.stdev, c_stdev)
+    
+    def test_p_values(self):
+        case = nd.NormalDistribution(10,1.0)
+        
+        self.assertEqual(case.p_value(None), 1.0)
+        # need to test the right cases here...
+    
+    def tearDown(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
--- src/normal_distribution.py
+++ src/normal_distribution.py
@@ -4,10 +4,49 @@ import sys
 import os.path
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
+import numpy as np
+from scipy.stats import norm
+from math import sqrt
 
+class NormalDistribution:
 
-
-
+    def __init__(self, timepoints, reps=1):
+        try:
+            if len(reps) == timepoints:
+                times = np.array(reps)
+            else:
+                times = reps[0] * np.ones(timepoints, dtype='float')
+            
+        except: # reps is not array-like
+            times = reps * np.ones(timepoints, dtype='float')
+        
+        self.stdev = self._compute_stdev(times)
+        self.max_score = self._compute_max_score(times)
+        self.expected = self.max_score / 2.0
+    
+    def _compute_max_score(self, times):
+        max_score = (sum(times)**2 - sum(times**2)) / 2.0
+        return max_score
+    
+    def _compute_stdev(self, times):
+        nn = sum(times)
+        ns = times
+        
+        var = (nn**2 * (2*nn + 3) - sum(ns**2 * (2*ns + 3))) / 72.0
+        sdv = sqrt(var)
+        return sdv
+    
+    def p_value(self, S):
+        if not S:
+            p = 1.0
+        else:
+            M = self.max_score
+            score = (abs(S) + M) / 2.0
+            p = 2 * norm.sf(-(score-0.5),
+                            loc=self.expected,
+                            scale=self.stdev)
+            p = 0.0
+        return p
 
 if __name__ == "__main__":
-    print "This is a jtk-cycle null distribution module."
+    print "This is a jtk-cycle null distribution normal-approximation module."
