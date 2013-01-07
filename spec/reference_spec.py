67cedb466d5351a7d7c3c85cbb4fec773295e5c0
andy c <t2ahc@sad-panda.local>
2013-01-07 11:42:32 -0600

    spec and add symmetry and function options to reference timeseries

--- spec/reference_spec.py
+++ spec/reference_spec.py
@@ -13,6 +13,60 @@ import reference as R
 import random
 import numpy as np
 
+class CustomFunctionSpec(unittest.TestCase):
+    """Describe Reference instances with custom functions."""
+
+    def setUp(self):
+        angle_to_time = 24.0 / (2 * np.pi)
+        self.reindex = lambda p: p * angle_to_time
+
+    def test_constant(self):
+        """It should make a series consisting of constant values."""
+        cv = 10. * np.random.random()
+        cf = lambda x: cv
+        case = R.Reference(np.arange(0,24,2, dtype='float'), 24., 0., function=cf)
+        for value in case.__values__:
+            self.assertEqual(value, cv)
+
+    def test_alternating(self):
+        """It should generate a series with alternating +/- 1 values."""
+        af = lambda p: -1 ** int(self.reindex(p))
+        case = R.Reference(np.arange(0,24,2, dtype='float'), 24., 0., function=af)
+        for i,value in enumerate(case.__values__):
+            self.assertEqual(value, -1**i)
+
+    def test_harmonic(self):
+        """It should test against a descending harmonic series."""
+        hf = lambda p: np.round(1.0 / (1.0 + self.reindex(p)), 2)
+        case = R.Reference(np.arange(0,24,1, dtype='float'), 24., 0., function=hf)
+        for i,value in enumerate(case.__values__):
+            self.assertEqual(np.round(1./(i+1.0), 2), value)
+
+class RankingSpec(unittest.TestCase):
+    """Describe the rank-ordering functions in time series class."""
+
+    def setUp(self):
+        self.case = R.Reference(range(0,24,2),24,0)
+
+    def test_rankings(self):
+        """It correctly rank-orders elements in a time-series."""
+        series = [5,3,2,1,4,6]
+        ranked = self.case.__rank__(series)
+        expect = [0.8, 0.4, 0.2, 0.0, 0.6, 1.0]
+        for p in zip(ranked, expect):
+            self.assertEqual(p[0], p[1])
+    
+    def test_fractional_rankings(self):
+        """It correctly fractionally ranks elements."""
+        series = [1,3,2,2,5,4]
+        ranked = self.case.__rank__(series)
+        expect = [0.0, 0.6, 0.3, 0.3, 1.0, 0.8]
+        for p in zip(ranked, expect):
+            self.assertEqual(p[0], p[1])
+
+    def tearDown(self):
+        pass
+
 class ReferenceSpec(unittest.TestCase):
     """Describe the reference time series class."""
     
@@ -50,22 +104,6 @@ class ReferenceSpec(unittest.TestCase):
         self.assertEqual(len(self.case.signs), 12)
         for s in self.case.signs:
             self.assertTrue(s in (-1,0,1))
-    
-    def test_rankings(self):
-        """It correctly rank-orders elements in a time-series."""
-        series = [5,3,2,1,4,6]
-        ranked = self.case.__rank__(series)
-        expect = [0.8, 0.4, 0.2, 0.0, 0.6, 1.0]
-        for p in zip(ranked, expect):
-            self.assertEqual(p[0], p[1])
-    
-    def test_fractional_rankings(self):
-        """It correctly fractionally ranks elements."""
-        series = [1,3,2,2,5,4]
-        ranked = self.case.__rank__(series)
-        expect = [0.0, 0.6, 0.3, 0.3, 1.0, 0.8]
-        for p in zip(ranked, expect):
-            self.assertEqual(p[0], p[1])
         
     def tearDown(self):
         pass
--- src/reference.py
+++ src/reference.py
@@ -16,14 +16,19 @@ class Reference:
         """Initializes a reference time-series."""
         self.period = period
         self.offset = offset
-        
+
         self.__xs__ = np.array(xs, dtype='float')
         self.__function__ = np.cos
+        self.__symmetry__ = True
+
         if "function" in kwargs.keys():
-            self.__function__ = kwargs["function"]
-        
+            self.__function__ = np.frompyfunc(kwargs["function"],1,1)
+            self.__symmetry__ = False
+        if "symmetry" in kwargs.keys():
+            self.__symmetry__ = kwargs["symmetry"]
+
         self.__values__ = self.__build_values__(period, offset)
-        
+
         # Public data handles.
         self.series = self.__rank__(self.__values__)
         self.signs = np.sign(self.__values__)
@@ -33,22 +38,23 @@ class Reference:
         """Evaluates the function f at argued xs."""
         f = self.__function__
         pi = round(np.pi,4)
-        
+        sf = 2. if self.__symmetry__ else 1.
+
         time_to_angle = 2 * pi / period
-        dx = (offset * time_to_angle) / 2.0
+        dx = (offset * time_to_angle) / sf
         xs = self.__xs__ * time_to_angle
-        
+
         values = np.array(f(xs + dx), dtype='float')
         return values
     
     def __rank__(self, data):
         """Fractional rank ordering of elements."""
         rank = lambda v: sorted(range(len(v)), key=v.__getitem__)
-        
+
         n = len(data)
         idxs=rank(data)
         sorts=[data[idx] for idx in idxs]
-        
+
         sum_ranks = 0
         n_ranked = 1
         f_ranks = [0]*n
