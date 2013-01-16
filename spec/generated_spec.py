79862e3a92e23b6ce40a4a50723cd3d2c868e3bb
andy c <t2ahc@scientific-panda.local>
2013-01-16 11:41:59 -0600

    respec generated distribution to perform multi-period test

--- spec/generated_spec.py
+++ spec/generated_spec.py
@@ -19,7 +19,9 @@ class DistributionSpec(unittest.TestCase):
     """Describe the Distribution class."""
     def setUp(self):
         self.series = series = np.random.random(TEST_N)
-        self.case = g.GeneratedDistribution(series, N=10)
+        self.case = g.GeneratedDistribution(N=10)
+        self.period = period = len(series)
+        self.case.add_reference(period, series)
     
     def test_initialization(self):
         """It should correctly generate an instance of the class."""
@@ -27,44 +29,49 @@ class DistributionSpec(unittest.TestCase):
     
     def test_series_memoization(self):
         """It should correctly recall the series initially used to generate."""
-        self.assertTrue(np.all(self.series == self.case.reference))
+        self.assertTrue(np.all(self.series == self.case.references[self.period]))
 
 class ScoresSpec(unittest.TestCase):
     """Describe the Monte-Carlo generation of scores."""
     
     def test_default_size(self):
-        """It defaults to a distribution size of 10000."""
+        """It defaults to a distribution size of 25000."""
         series = np.random.random(TEST_N)
-        case = g.GeneratedDistribution(series)
+        case = g.GeneratedDistribution()
+        period = len(series)
+        case.add_reference(period, series)
         
-        self.assertEqual(10000, case.scores.size)
-        self.assertTrue(np.all(np.abs(case.scores) <= MAX_S))
+        self.assertEqual(25000, case.scores[period].size)
+        self.assertTrue(np.all(np.abs(case.scores[period]) <= MAX_S))
         
     def test_argued_size(self):
         """It accepts an argued distribution size and build accordingly."""
         distribution_size = 25
         series = np.random.random(TEST_N)
-        case = g.GeneratedDistribution(series, N=distribution_size)
+        period = len(series)
+        case = g.GeneratedDistribution(N=distribution_size)
+        case.add_reference(len(series), series)
         
-        self.assertEqual(distribution_size, case.scores.size)
-        self.assertTrue(np.all(np.abs(case.scores) <= MAX_S))
+        self.assertEqual(distribution_size, case.scores[period].size)
+        self.assertTrue(np.all(np.abs(case.scores[period]) <= MAX_S))
 
 class SeriesSpec(unittest.TestCase):
     """Describe the generation of test series."""
     def setUp(self):
         self.distribution_size = ds = 10
         self.series = series = np.random.random(TEST_N)
-        self.case = g.GeneratedDistribution(series, N=ds)
+        self.case = g.GeneratedDistribution(N=ds)
+        self.case.add_reference(len(series), series)
     
     def test_series_shape(self):
         """It generates a test series of appropriate shape."""
-        series = self.case.__make_series__()
+        series = self.case.__make_series__(TEST_N)
         self.assertEqual(self.distribution_size, series.shape[0])
         self.assertEqual(TEST_N, series.shape[1])
     
     def test_series_values(self):
         """It should generate values with appropriate bounds."""
-        series = self.case.__make_series__()
+        series = self.case.__make_series__(TEST_N)
         shape = (self.distribution_size, TEST_N)
         self.assertTrue(np.all(series <= np.ones(shape)))
         self.assertTrue(np.all(series >= np.zeros(shape)))
@@ -72,17 +79,20 @@ class SeriesSpec(unittest.TestCase):
 class PValueSpec(unittest.TestCase):
     """Describe the computation of p-values."""
     def setUp(self):
-        self.case = g.GeneratedDistribution(np.random.random(TEST_N), N=2500)
+        series = np.random.random(TEST_N)
+        self.period = period = len(series)
+        self.case = g.GeneratedDistribution(N=2500)
+        self.case.add_reference(period, series)
     
     def test_minimum(self):
         """It should report 1.0 for a zero score."""
         expect = 1.0
-        actual = self.case.p_value(0)
+        actual = self.case.p_value(0, period=self.period)
         self.assertEqual(expect, actual)
     
     def test_maximum(self):
         """It should report significant values for a high score."""
-        actual = self.case.p_value(MAX_S)
+        actual = self.case.p_value(MAX_S, period=self.period)
         significant = 0.01
         self.assertTrue(actual <= significant)
     
--- src/generated.py
+++ src/generated.py
@@ -10,26 +10,39 @@ import statistic as s
 class GeneratedDistribution:
     """Class represents a Monte-Carlo generated distribution for Kendall's Tau."""
 
-    def __init__(self, reference, **kwargs):
-        self.reference = reference
-        self.N = 10000
+    def __init__(self, **kwargs):
+        self.N = 25000
         if "N" in kwargs.keys():
             self.N = kwargs["N"]
-        self.scores = self.__make_scores__()
-    
-    def __make_scores__(self):
-        series = self.__make_series__()
+
+        self.references = {}
+        self.scores = {}
+
+    def add_reference(self, period, series):
+        self.references[period] = series
+        self.scores[period] = self.__make_scores__(period)
+
+    def __make_scores__(self, period):
+        series = self.__make_series__(period)
         scores = np.zeros(self.N)
-        for i,test in enumerate(series):
-            scores[i] = s.k_score(self.reference, test)
+
+        reference = self.references[period]
+        for i in xrange(series.shape[0]):
+            test = series[i,:]
+            scores[i] = s.k_score(reference, test)
+
         return np.abs(scores)
-    
-    def __make_series__(self):
-        n = self.reference.size
+
+    def __make_series__(self, period):
+        n = self.references[period].size
         return np.random.random((self.N, n))
-    
+
     def p_value(self, S, **kwargs):
-        leqs = np.sum(np.ones(self.N)[self.scores >= np.abs(S)])
+        try:
+            period = kwargs["period"]
+        except:
+            raise Exception("Must specify period for Monte Carlo distributions.")
+        leqs = np.sum(np.ones(self.N)[self.scores[period] >= np.abs(S)])
         return leqs / float(self.N)
     
 if __name__ == "__main__":
